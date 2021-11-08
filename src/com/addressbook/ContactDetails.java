7057caa80f8adef52684fb6eae49e2321723e3e3
T RAVI KUMAR <91825694+ravikumar990@users.noreply.github.com>
2021-11-08 23:13:38 +0530

    Added : Code for multiple AddressBook

--- src/com/addressbook/ContactDetails.java
+++ src/com/addressbook/ContactDetails.java
@@ -1,16 +1,65 @@
 package com.addressbook;
 
-public class ContactDetails extends PersonDetails  {
-	public static void main(String[] args) {
-		ContactDetails onePersonDetails = new ContactDetails();
-		onePersonDetails.getName();
-		onePersonDetails.getLastName();
-		onePersonDetails.getAddress();
-		onePersonDetails.getStateName();
-		onePersonDetails.getPhoneNumber();
-		onePersonDetails.getEmail();
-		onePersonDetails.getZipCode();
-		System.out.println(onePersonDetails.toString());
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class ContactDetails {
+
+	static Dictionary dictionary = new Hashtable();
+	static Hashtable<Integer, ArrayList<AddContact>> hashTable = new Hashtable<Integer, ArrayList<AddContact>>();
+
+	private static char inputCharater() {
+		Scanner scanCharater = new Scanner(System.in);
+		char charaterInput = scanCharater.next().charAt(0);
+		return charaterInput;
 	}
 
-}
+	private static int inputInteger() {
+		Scanner scanCharater = new Scanner(System.in);
+		int integerInput = scanCharater.nextInt();
+		return integerInput;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("enter the address book limit");
+		int addressBookLimit = inputInteger();
+		for (int i = 1; i <= addressBookLimit; i++) {
+			ArrayList<AddContact> addDetails = new <AddContact>ArrayList();
+			while (true) {
+				AddContact addPersonDetails = new AddContact();
+				System.out.printf(
+						"Input the Charater A to add Details E to Edit details D to delete details\n Any charater to Ignore");
+				char charater = inputCharater();
+				if (charater == 'A' || charater == 'E' || charater == 'D' || charater == 'a' || charater == 'e'
+						|| charater == 'd') {
+					switch (charater) {
+					case 'A':
+						addPersonDetails.setContactDetails();
+						addDetails.add(addPersonDetails);
+						break;
+					case 'E':
+						System.out.println("Enter the name to edit");
+						addPersonDetails.editDetails(addDetails);
+						break;
+					case 'D':
+						addPersonDetails.deleteDetails(addDetails);
+						break;
+					default:
+						System.out.println("you have not perform any operation");
+						break;
+					}
+
+				} else {
+					System.out.println("Details are uptodate");
+					break;
+				}
+			}
+			hashTable.put(i, addDetails);
+		}
+		for(int i=1; i<= hashTable.size(); i++) {
+			System.out.println("address book "+i);
+			System.out.println(hashTable.get(i));	
+		}		
+	}

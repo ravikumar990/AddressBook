1944515babd157dba0b1ccd6c5617e60cc574ca3
ravi <ravi@gmail.com>
2021-11-08 21:12:58 +0530

    Added welcome message

---
+++ src/com/addressbook/PersonDetails.java
@@ -0,0 +1,10 @@
+package com.addressbook;
+
+public class PersonDetails {
+	
+	public static void main(String[] args) {
+		System.out.println("**********Welcome to Address Book Program***********");
+
+	}
+
+}
